2bae798957008e0beb4370f40dc402af69806b97
Bah <e2233003@cmaisonneuve.qc.ca>
2025-04-08 11:33:30 -0400

    Fonctionnement des boutons de l'api

--- js/destination.js
+++ js/destination.js
@@ -1,31 +1,38 @@
-(function(){
-    console.log("04/06/27")
-    
-    const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+(function(){   
+    let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
     const domaine = window.location.href
-    let apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
     const categorie__ul__li= document.querySelectorAll(".categorie__ul__li");
-        console.log("categorie__ul__li",categorie__ul__li.length);
-    
+    // const titre_accordeon = document.querySelectorAll(".titre_accordeon");
+
+    mon_fetch(apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`);
     categorie__ul__li.forEach(li => {
-        li.addEventListener("click", function(){
-            console.log(li.dataset.id);
+        li.addEventListener("mousedown", function(){
             categoryId = li.dataset.id;
             apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
+            mon_fetch(apiUrl);
         })
     })
+    // titre_accordeon.forEach(h3 =>{
+    //     h3.addEventListener("mousedown", function(){
+    //         console.log("test")
+    //     })
+    // })
+    function mon_fetch(apiUrl){
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const destinationList = document.querySelector('.destination__list');
+            let destinationList = document.querySelector('.destination__list');
+            destinationList.innerHTML = "";
             data.forEach(article => {
                 const articleElement = document.createElement('div');
                 articleElement.innerHTML = `
-
-                    <a href="${article.link}">${article.title.rendered}</a>
+                    <h3 class="titre_accordeon">${article.title.rendered}</h3>
+                    <p>${article.excerpt.rendered}</p>
+                    <a href="${article.link}">Lire plus</a>
                 `;
                 destinationList .appendChild(articleElement);
             });
         })
         .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+    }
 })()
