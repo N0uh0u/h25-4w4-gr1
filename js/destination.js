85287b99a9472ed3501c759f22f16150b859a33b
Bah <e2233003@cmaisonneuve.qc.ca>
2025-04-10 11:42:46 -0400

    Avancement du css de l'accordéon

--- js/destination.js
+++ js/destination.js
@@ -20,14 +20,15 @@
             destinationList.innerHTML = "";
             data.forEach(article => {
                 const articleElement = document.createElement('div');
+                articleElement.classList.add('destination__div');
                 articleElement.innerHTML = `
-                    <h3>${article.title.rendered}</h3>
-
+                    <h3 class="destination__h3">${article.title.rendered}</h3>
+                    <label class="destination__label" for="rad-${article.id}"><img src="https://s2.svgbox.net/hero-solid.svg?ic=dots-horizontal&color=000" width="32" height="32">
                     <input type="radio" id="rad-${article.id}" name="destination" class="destination__rad">
 
                     <div class="destination__p"${article.excerpt.rendered}
                         <a class="destination__a" href="${article.link}">
-                            ...
+                            <img src="https://s2.svgbox.net/hero-solid.svg?ic=dots-horizontal&color=000" width="32" height="32">
                         </a>
                     </div>
                 `;
