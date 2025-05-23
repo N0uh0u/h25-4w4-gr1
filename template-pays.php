3a03cf530dab55901d4c54b644e6cafbaad2a182
N0uh0u <Nouhoubah05@gmail.com>
2025-05-23 14:11:20 -0400

    Ajustement du css

--- template-pays.php
+++ template-pays.php
@@ -17,13 +17,14 @@ Template Name: Template Événement
     }
     ?>
     </div>
+    <section class="destination">
+        <h2 class="destination__titre"> Article</h2>
+        <div class="destination__list">
+            <!-- add dataset data-method=categories -->
+        </div>
+    </section>
     <div class="vague__pays">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#0099ff" fill-opacity="1" d="M0,128L120,117.3C240,107,480,85,720,90.7C960,96,1200,128,1320,144L1440,160L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
-    </div>
-<section class="destination">
-    <h2 class="destination__titre"> <?php echo "nom du pays" ?> </h2>
-    <div class="destination__list">
-        <!-- add dataset data-method=categories -->
     </div>
 <?php get_footer(); ?>
