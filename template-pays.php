1c048308ff7a6d22fbd5208f564588729fafe0ba
Bah <e2233003@cmaisonneuve.qc.ca>
2025-05-23 11:07:06 -0400

    ef ajout des pays dans la liste

--- template-pays.php
+++ template-pays.php
@@ -3,14 +3,23 @@
 Template Name: Template Événement
 */
 ?>
+
 <?php get_header(); ?>
     <h2><?php the_title(); ?></h2>
     <?php the_content() ?>
-    <p>Nom pays: <?php the_field('nom_pays')?> </p>
-
-
-    <section class="destination">
-        <h2 class="destination__titre">Articles de la catégorie</h2>
-        <!-- <div class="destination__list"></div> -->
-    </section>
+<div>
+    <?php 
+    $pays =["France", "États-Unis", "Canada", "Argentine", "Chili", "Belgique", "Maroc", "Mexique", "Japon", "Italie", "Islande", "Chine", "Grèce", "Suisse"];
+    if(!empty($pays)){
+        echo '<ul class="categorie__ul__pays">';
+        foreach($pays as $ps){
+            echo '<li  data-id="' . $ps . '" class="categorie__ul__li">' . $ps. '</li>';
+        }
+    }
+    ?>
+    </div>
+    <div class="vague__pays">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#0099ff" fill-opacity="1" d="M0,128L120,117.3C240,107,480,85,720,90.7C960,96,1200,128,1320,144L1440,160L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+    </div>
 <?php get_footer(); ?>
